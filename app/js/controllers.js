683c179288a1428312407ce6704dd98906df178b
albertcui <albertqcui+github@gmail.com>
2014-11-16 22:14:54 +0000

    add loading gif

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,26 @@
 var foodApp = angular.module('foodApp', ['geolocation'])
 
-foodApp.controller('FoodController', function($scope, $http, geolocation) {
+foodApp.directive('loading', function () {
+      return {
+        restrict: 'E',
+        replace:true,
+        template: '<div class="loading"><img src="http://www.nasa.gov/multimedia/videogallery/ajax-loader.gif" width="20" height="20" />LOADING...</div>',
+        link: function (scope, element, attr) {
+              scope.$watch('loading', function (val) {
+                  if (val)
+                      $(element).show();
+                  else
+                      $(element).hide();
+              });
+        }
+      }
+  }).controller('FoodController', function($scope, $http, geolocation) {
    
     $scope.findStuff = function() {
+        $scope.loading = true
         geolocation.getLocation().then(function(data){
+
+            console.log($scope.loading)
             $scope.lat = data.coords.latitude
             $scope.long = data.coords.longitude
             $http({
@@ -12,9 +29,11 @@ foodApp.controller('FoodController', function($scope, $http, geolocation) {
                 params: {lat: data.coords.latitude, long: data.coords.longitude }
             })
             .success(function(data, status, headers, config){
+                $scope.loading = false;
                 $scope.data =  data.businesses;
             })
             .error(function(data, status, headers, config){
+                $scop.loading = false;
                 return "ERROR!"
             })
           
